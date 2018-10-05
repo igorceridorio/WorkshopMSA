9f1bd7d39d454a7bf1bfae8940523655c1aca0ee
igorceridorio <igorceridorio@gmail.com>
2018-10-05 14:04:56 -0300

    Implementação de serviço de recuperação de taxa de juros por produto

--- taxa-juros-microservice/src/main/java/br/com/workshopmsa/taxajuros/controller/TaxaJurosController.java
+++ taxa-juros-microservice/src/main/java/br/com/workshopmsa/taxajuros/controller/TaxaJurosController.java
@@ -6,11 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.workshopmsa.taxajuros.database.domain.TaxaJuros;
 import br.com.workshopmsa.taxajuros.database.repository.TaxaJurosRepository;
 import br.com.workshopmsa.taxajuros.domain.TaxaJurosModel;
+import br.com.workshopmsa.taxajuros.domain.TaxaJurosResponse;
 import br.com.workshopmsa.taxajuros.domain.TaxasJurosResponse;
 
 @RestController
@@ -23,13 +26,18 @@ public class TaxaJurosController {
 	@Autowired
 	private TaxaJurosRepository taxaJurosRepository;
 	
-	@GetMapping("/")
-	public TaxasJurosResponse getTaxasJuros() {
+	@GetMapping("")
+	public TaxasJurosResponse getTaxasJuros() throws Exception {
 		
-		List<TaxaJurosModel> listaTaxasJuros = new ArrayList<>();
+		List<TaxaJurosModel> listaTaxasJurosModel = new ArrayList<>();
+		List<TaxaJuros> taxasJuros = taxaJurosRepository.findAll();
 		
-		taxaJurosRepository.findAll().forEach(tx -> {
-			listaTaxasJuros.add(TaxaJurosModel
+		if (taxasJuros == null) {
+			throw new Exception("Os dados dos produtos não foram localizados");
+		}
+		
+		taxasJuros.forEach(tx -> {
+			listaTaxasJurosModel.add(TaxaJurosModel
 					.builder()
 					.idProduto(tx.getIdProduto())
 					.jurosAm(tx.getJurosAm())
@@ -38,7 +46,27 @@ public class TaxaJurosController {
 		
 		return TaxasJurosResponse
 				.builder()
-				.listaTaxasJuros(listaTaxasJuros)
+				.listaTaxasJuros(listaTaxasJurosModel)
+				.porta(Integer.parseInt(environment.getProperty("local.server.port")))
+				.build();
+	}
+	
+	@GetMapping("/{idproduto}")
+	public TaxaJurosResponse getTaxaJuros(@PathVariable("idproduto") Long idProduto) throws Exception {
+
+		TaxaJuros taxaJuros = taxaJurosRepository.findByIdProduto(idProduto);
+		
+		if (taxaJuros == null) {
+			throw new Exception("Produto não localizado na base de dados");
+		}
+		
+		return TaxaJurosResponse
+				.builder()
+				.taxaJuros(TaxaJurosModel
+						.builder()
+						.idProduto(taxaJuros.getIdProduto())
+						.jurosAm(taxaJuros.getJurosAm())
+						.build())
 				.porta(Integer.parseInt(environment.getProperty("local.server.port")))
 				.build();
 	}
