01de198709aced1ebf6cebd6e0247858d9e518b6
igorceridorio <igorceridorio@gmail.com>
2018-10-09 19:18:22 -0300

    Criação da base da etapa 3 (Eureka)

---
+++ 3.eureka/taxa-juros-microservice/src/main/java/br/com/workshopmsa/taxajuros/domain/TaxaJurosResponse.java
@@ -0,0 +1,20 @@
+package br.com.workshopmsa.taxajuros.domain;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TaxaJurosResponse {
+	
+	//private TaxaJurosModel taxaJuros;
+	private Long idProduto;
+	private BigDecimal jurosAm;
+	private int porta;
+}
