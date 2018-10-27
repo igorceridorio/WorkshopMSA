ac5b8238630bbee459553124e95fe90eff93ffa8
igorceridorio <igorceridorio@gmail.com>
2018-10-27 15:41:51 -0300

    Revertendo mudanças

--- 7.apresentacao/emprestimo-microservice/src/main/java/br/com/workshopmsa/emprestimo/proxy/TaxaJurosServiceProxy.java
+++ 7.apresentacao/emprestimo-microservice/src/main/java/br/com/workshopmsa/emprestimo/proxy/TaxaJurosServiceProxy.java
@@ -1,15 +1,12 @@
 package br.com.workshopmsa.emprestimo.proxy;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.workshopmsa.emprestimo.domain.EmprestimoModel;
 
-//@FeignClient(name = "taxa-juros-microservice", url = "http://localhost:8000")
-@FeignClient(name = "taxa-juros-microservice")
-@RibbonClient(name = "taxa-juros-microservice")
+@FeignClient(name = "taxa-juros-microservice", url = "http://localhost:8000")
 public interface TaxaJurosServiceProxy {
 
 	@GetMapping("taxasjuros/{idproduto}")
--- 7.apresentacao/taxa-juros-microservice/src/main/java/br/com/workshopmsa/taxajuros/TaxaJurosMicroserviceApplication.java
+++ 7.apresentacao/taxa-juros-microservice/src/main/java/br/com/workshopmsa/taxajuros/TaxaJurosMicroserviceApplication.java
@@ -2,10 +2,8 @@ package br.com.workshopmsa.taxajuros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class TaxaJurosMicroserviceApplication {
 
 	public static void main(String[] args) {
