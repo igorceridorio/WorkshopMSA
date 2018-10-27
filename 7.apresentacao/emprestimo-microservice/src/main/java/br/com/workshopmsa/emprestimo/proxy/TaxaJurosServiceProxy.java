f7bd427ff7977689b5c126d112b6fc999bdb33f2
igorceridorio <igorceridorio@gmail.com>
2018-10-27 16:07:46 -0300

    Prática até Eureka

--- 7.apresentacao/emprestimo-microservice/src/main/java/br/com/workshopmsa/emprestimo/proxy/TaxaJurosServiceProxy.java
+++ 7.apresentacao/emprestimo-microservice/src/main/java/br/com/workshopmsa/emprestimo/proxy/TaxaJurosServiceProxy.java
@@ -1,12 +1,14 @@
 package br.com.workshopmsa.emprestimo.proxy;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.workshopmsa.emprestimo.domain.EmprestimoModel;
 
-@FeignClient(name = "taxa-juros-microservice", url = "http://localhost:8000")
+@FeignClient(name = "taxa-juros-microservice")  
+@RibbonClient(name = "taxa-juros-microservice")
 public interface TaxaJurosServiceProxy {
 
 	@GetMapping("taxasjuros/{idproduto}")
